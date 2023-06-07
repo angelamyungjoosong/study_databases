0e24a253c57f0379247abeb48f884fdd5b832b45
angelamyungjoosong <angelaforever122@gmail.com>
2023-06-07 15:42:40 +0900

    london

--- selectwhereRangeAndEquals.sql
+++ selectwhereRangeAndEquals.sql
@@ -1,6 +1,6 @@
 
 -- source: https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_where
--- 조건 : CustomerID가 50에서 89이고, City가 Berlin인 고객
+-- 조건 : CustomerID가 50에서 89이고, City가 London인 고객
 SELECT * 
 FROM Customers
-WHERE CustomerID BETWEEN 50 AND 89 AND City = 'Berlin' ;
+WHERE CustomerID BETWEEN 50 AND 89 AND City = 'London' ;
