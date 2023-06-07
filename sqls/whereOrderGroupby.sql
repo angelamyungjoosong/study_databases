008bef6895b89ad75d0a09b775e3232eb20d1013
angelamyungjoosong <angelaforever122@gmail.com>
2023-06-07 17:40:02 +0900

    수정

--- sqls/whereOrderGroupby.sql
+++ sqls/whereOrderGroupby.sql
@@ -1,5 +1,5 @@
 -- 출처: https://www.w3schools.com/sql/trysql.asp?filename=trysql_is_not_null
-SELECT COUNT(*)
+SELECT COUNT(*)EmployeeID
 FROM ORDERS
 WHERE EmployeeID IN (7, 9)
 GROUP BY EmployeeID;
